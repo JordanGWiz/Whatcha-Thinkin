473d871c5f68674d24519ad7d575ca0006b97030
Jordan <jordan.gonzales17@hotmail.com>
2024-04-25 17:29:33 -0700

    Added a localStorage to the darkmode feature

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,13 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("Document is ready!");
   const checkBox = document.querySelector('.switch input[type="checkbox"]');
 
+  if (localStorage.getItem("darkMode") === "true") {
+    checkBox.checked = true;
+    document.body.classList.add("dark-mode");
+  }
+
   checkBox.addEventListener("change", function () {
     if (this.checked) {
       console.log("Switched to dark mode");
       document.body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "true");
     } else {
       console.log("Switched to light mode");
       document.body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "false");
     }
   });
 });
