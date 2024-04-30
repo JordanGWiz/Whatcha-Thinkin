ae376debfd1efb45c9d423d3f89709155f48a52d
Jordan <jordan.gonzales17@hotmail.com>
2024-04-29 17:55:55 -0700

    Added comments

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,4 @@
+// Select and store elements into variables for easier access.
 const usernameEl = document.querySelector("#username");
 const titleEl = document.querySelector("#title");
 const contentEl = document.querySelector("#textarea");
@@ -8,7 +9,7 @@ document
   .getElementById("post-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
+    // Checks if the username, title, or content are empty.
     if (
       usernameEl.value === "" ||
       titleEl.value === "" ||
@@ -16,15 +17,18 @@ document
     ) {
       alert("Please fill out your username, title, and blog text");
     } else {
+      // Create an object to represent the new blog post.
       const blogPost = {
         username: usernameEl.value,
         title: titleEl.value,
         content: contentEl.value,
       };
+
+      // Add the new blog post to the collection of posts.
       postCollection.push(blogPost);
-
+      // Update the 'post' item in localStorage with the new collection of posts.
       localStorage.setItem("post", JSON.stringify(postCollection));
-
+      // Redirect the user to 'blog.html' after submission.
       window.location.replace("blog.html");
     }
   });
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,23 @@
+//  Set up an event listener for the 'DOMContentLoaded' event, which triggers once the entire HTML document is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Document is ready!");
   const checkBox = document.querySelector('.switch input[type="checkbox"]');
 
+  // Checks if 'darkMode' is stored in localStorage and is set to "true".
   if (localStorage.getItem("darkMode") === "true") {
     checkBox.checked = true;
     document.body.classList.add("dark-mode");
   }
 
+  // Adds an event listener to the checkbox for the 'change' event.
   checkBox.addEventListener("change", function () {
     if (this.checked) {
+      // If the checkbox is checked
       console.log("Switched to dark mode");
       document.body.classList.add("dark-mode");
       localStorage.setItem("darkMode", "true");
     } else {
+      // If the checkbox is not checked
       console.log("Switched to light mode");
       document.body.classList.remove("dark-mode");
       localStorage.setItem("darkMode", "false");
